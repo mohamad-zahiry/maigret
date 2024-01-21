e47eb023a8f39c0b065564e65e07cf706987a830
Mohamad Zahiri <mamadzhri@gamil.com>
2024-01-21 20:48:01 +0330

    chore: add `ksp` plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     kotlin("plugin.serialization").version("1.9.10")
+    id("com.google.devtools.ksp")
 }
 
 android {
