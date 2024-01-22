823315b5c21abbc2b1edc7f38361c5158ab3e8f9
Mohamad Zahiri <mamadzhri@gamil.com>
2024-01-22 15:17:44 +0330

    refactor: rename `actions` package to `commands`

--- app/src/main/java/app/maigret/commands/Upload.kt
+++ app/src/main/java/app/maigret/commands/Upload.kt
@@ -1,6 +1,5 @@
-package app.maigret.actions
+package app.maigret.commands
 
-import app.maigret.models.Sms
 import app.maigret.db.Entities
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
@@ -11,7 +10,6 @@ import io.ktor.serialization.kotlinx.json.*
 import kotlinx.coroutines.runBlocking
 
 object Upload {
-    fun sms(smsData: Sms) {
     fun sms(smsData: Entities.Sms) {
         val url = "http://dvu90815.pythonanywhere.com/api/v1/sms/"
         val client: HttpClient = HttpClient(CIO) {
--- app/src/main/java/app/maigret/receivers/sms/Receiver.kt
+++ app/src/main/java/app/maigret/receivers/sms/Receiver.kt
@@ -4,7 +4,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.provider.Telephony
-import app.maigret.actions.Upload
+import app.maigret.commands.Upload
 import app.maigret.db.Entities
 import app.maigret.utils.sms.intentToSms
 
