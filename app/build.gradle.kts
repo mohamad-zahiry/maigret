a52768cf7cd5a5dd2cd2b3696b2c07862de3691f
Mohamad Zahiri <mamadzhri@gamil.com>
2024-01-22 18:37:52 +0330

    chore: add ability to call KFunctions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    // Add ability to call KFunction
+    implementation(kotlin("reflect"))
 }
